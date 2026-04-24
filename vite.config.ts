6fd2f6489e1c37b837304a47b6eac327d33eac64
Badhon303 <badhon.alam303@gmail.com>
2026-04-24 20:18:44 +0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger"
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 3003,
+    port: 3000,
     hmr: {
       overlay: false,
     },
